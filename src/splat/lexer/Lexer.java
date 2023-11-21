c3d0d7a86d020fa63036d897c02bf1a39c83ddae
ArmanPrmanov <arman.prmanov@nu.edu.kz>
2023-11-22 00:17:46 +0600

    Bitty version

--- src/splat/lexer/Lexer.java
+++ src/splat/lexer/Lexer.java
@@ -6,13 +6,25 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Lexer {
 	private File progFile;
 
 	private List<String> StatementTokenList = Arrays.asList("if", "while", "begin", "end", "program", "do", "return", "then", "is", "print", "print_line");
 	private List<String> OperatorTokenList = Arrays.asList("and", "or", "not", ">", "<", ">=", "<=", "=", "==", "-", "+", "*", "/", "%");
-	private List<String> SpecialCharTokenList = Arrays.asList("(", ")", ";", ":", ",", "\"", "!");
+	//private List<String> SpecialCharTokenList = Arrays.asList("(", ")", ";", ":", ",", "\"", "!");
+
+	private List<String> SpecialCharTokenList = Arrays.asList("(", ")", ";", ":", ",");
+
+	private enum LexState
+	{
+		kInit,
+		kFormValue,
+		kFormStringValue,
+		kFormTwoCharOperator,
+		kFinalizeValue
+	}
 
 	public Lexer(File progFile) {
 		this.progFile = progFile;
@@ -32,42 +44,120 @@ public class Lexer {
 			int currTokenColumn = 0;
 			StringBuilder tokenValue = new StringBuilder();
 
+			LexState state = LexState.kInit;
+
 			while ((currentChar = reader.read()) != -1) {
 				column++;
 				char currentCharValue = (char) currentChar;
 
-				if (currentCharValue == '\n' || currentCharValue == '\r') {
-					line++;
-					column = 0;
-					continue;
-				}
+				boolean should_get_next_c = false;
 
+//				while (!should_get_next_c) {
 
-				if (currentCharValue == ' ' || currentCharValue == '\t') {
-					if (tokenValue.length() > 0) {
-						tokens.add(new Token(tokenValue.toString(), currTokenLine, currTokenColumn));
+					if (state == LexState.kInit) {
 						tokenValue = new StringBuilder();
+
+						if (Character.isLetterOrDigit(currentCharValue))
+						{
+							state = LexState.kFormValue;
+						}
+						else if (currentCharValue == '"')
+						{
+							state = LexState.kFormStringValue;
+						}
+						else if (currentCharValue == '=' || currentCharValue == '>' || currentCharValue == '<')
+						{
+							tokenValue = new StringBuilder(currentCharValue);
+							state = LexState.kFormTwoCharOperator;
+							should_get_next_c = true;
+						}
+						else if (isOperator(currentCharValue) || isSpecialChar(currentCharValue))
+						{
+							tokenValue = new StringBuilder(currentCharValue);
+							state = LexState.kFinalizeValue;
+							should_get_next_c = true;
+						}
+						else if (currentCharValue == ' ' || currentCharValue == '\t')
+						{
+							should_get_next_c = true;
+						}
+						else if (currentCharValue == '\n' || currentCharValue == '\r')
+						{
+							tokenValue = new StringBuilder();
+							should_get_next_c = true;
+							line++;
+							column = 0;
+						}
+						else
+						{
+							throw new LexException(currentCharValue + "", line, column);
+						}
+					} else if (state == LexState.kFormValue) {
+						if (Character.isLetterOrDigit(currentCharValue))
+						{
+							tokenValue.append(currentCharValue);
+							should_get_next_c = true;
+						}
+						else
+						{
+							state = LexState.kFinalizeValue;
+						}
+					} else if (state == LexState.kFormStringValue) {
+						tokenValue.append(currentCharValue);
+						if (currentCharValue == '"')
+						{
+							state = LexState.kFinalizeValue;
+						}
+						else if (currentCharValue == '\n' || currentCharValue == '\r')
+						{
+							throw new LexException(currentCharValue + "", line, column);
+						}
+					} else if (state == LexState.kFormTwoCharOperator) {
+						if (currentCharValue == '=')
+						{
+							tokenValue.append(currentCharValue);
+						}
+						state = LexState.kFinalizeValue;
+					} else if (state == LexState.kFinalizeValue) {
+						if (tokenValue.length() > 0) {
+							tokens.add(new Token(tokenValue.toString(), line, column));
+						}
+						state = LexState.kInit;
 					}
 				}
-				else if (isSpecialChar(currentCharValue)){
-					if (tokenValue.length() > 0) {
-						tokens.add(new Token(tokenValue.toString(), currTokenLine, currTokenColumn));
-						tokenValue = new StringBuilder();
-					}
-					tokens.add(new Token(currentCharValue + "", currTokenLine, currTokenColumn));
-					tokenValue = new StringBuilder();
-				}
-				else if (isValidCharacter(currentCharValue) ||
-						isOperator(currentCharValue)) {
-					if (tokenValue.length() == 0) {
-						currTokenLine = line;
-						currTokenColumn = column;
-					}
-					tokenValue.append(currentCharValue);
-				} else {
-					throw new LexException(currentCharValue + "", line, column);
-				}
-			}
+//			}
+
+//				if (currentCharValue == '\n' || currentCharValue == '\r') {
+//					line++;
+//					column = 0;
+//					continue;
+//				}
+//
+//
+//				if (currentCharValue == ' ' || currentCharValue == '\t') {
+//					if (tokenValue.length() > 0) {
+//						tokens.add(new Token(tokenValue.toString(), currTokenLine, currTokenColumn));
+//						tokenValue = new StringBuilder();
+//					}
+//				}
+//				else if (isSpecialChar(currentCharValue)){
+//					if (tokenValue.length() > 0) {
+//						tokens.add(new Token(tokenValue.toString(), currTokenLine, currTokenColumn));
+//						tokenValue = new StringBuilder();
+//					}
+//					tokens.add(new Token(currentCharValue + "", currTokenLine, currTokenColumn));
+//					tokenValue = new StringBuilder();
+//				}
+//				else if (isValidCharacter(currentCharValue) ||
+//						isOperator(currentCharValue)) {
+//					if (tokenValue.length() == 0) {
+//						currTokenLine = line;
+//						currTokenColumn = column;
+//					}
+//					tokenValue.append(currentCharValue);
+//				} else {
+//					throw new LexException(currentCharValue + "", line, column);
+//				}
 
 			if (tokenValue.length() > 0) {
 				tokens.add(new Token(tokenValue.toString(), currTokenLine, currTokenColumn));
